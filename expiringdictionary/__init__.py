c248dc9e8db153c058c982cd8702c415609ecedd
root <root@localhost>
2023-05-30 08:48:07 -0400

    yeah

--- expiringdictionary/__init__.py
+++ expiringdictionary/__init__.py
@@ -18,7 +18,7 @@ class ExpiringDictionary:
 
     async def delete(self,key:str,value:Any):
         if key in self.dict:
-			self.dict.pop(key)
+            self.dict.pop(key)
             return 1
         else:
             return 0
